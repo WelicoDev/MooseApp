a0158c07de5aebb0a24d6bb8e91569af60d0b01d
o.xuramov@uic.group <o.xuramov@uic.group>
2025-03-13 16:30:06 +0500

    updated project (.gitignore added)

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'welicodevpro@gmail.com'
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
